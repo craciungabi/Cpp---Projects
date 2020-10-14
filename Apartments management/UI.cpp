194e7c6922adecfedc7fe68e773653b7a4f9211a
craciungabi <62244698+craciungabi@users.noreply.github.com>
2020-10-14 13:35:10 +0300

    Update UI.cpp

--- Apartments management/UI.cpp
+++ Apartments management/UI.cpp
@@ -5,29 +5,29 @@ using namespace std;
 
 void addCheltuiala(Service& s) {
 	Cheltuiala c;
-	cout << "Dati cheltuiala: ";
+	cout << "Give the expense: ";
 	cin >> c;
 	s.addCheltuiala(c);
-	cout << "Cheltuiala a fost adaugata cu succes!" << endl;
+	cout << "The expense was added successfully!" << endl;
 }
 
 void delCheltuiala(Service& serv) {
 	int id;
-	cout << "Dati id-ul cheltuielii de sters: ";
+	cout << "Give the expense ID to deletes: ";
 	cin >> id;
 	serv.delCheltuiala(id);
-	cout << "Cheltuiala a fost  stearsa cu succes!" << endl;
+	cout << "The expense was successfully deleted!" << endl;
 }
 
 void updateCheltuiala(Service& serv) {
 	int id, new_zi, new_suma;
 	char* new_tip = new char[10];
-	cout << "Dati id-ul cheltuielii de modificat: "; cin >> id;
-	cout << "Dati noua zi a cheltuielii: "; cin >> new_zi;
-	cout << "Dati noua suma a cheltuielii: "; cin >> new_suma;
-	cout << "Dati noul tip al cheltuielii: "; cin >> new_tip;
+	cout << "Give the id of the expense to change: "; cin >> id;
+	cout << "Give the new day of spending: "; cin >> new_zi;
+	cout << "Give the new amount of the expense: "; cin >> new_suma;
+	cout << "Enter the new type of expenditure: "; cin >> new_tip;
 	serv.update(id, new_zi, new_suma, new_tip);
-	cout << " Cheltuiala a fost modificata cu succes!" << endl;
+	cout << " The expense has been successfully changed!" << endl;
 }
 
 void showAll(Service& serv) {
@@ -59,11 +59,11 @@ void afisare_mancare_mai_mare_5(Service& serv) {
 }
 
 void ui_suma_mancare(Service& serv) {
-	cout << "Suma cheltuielilor pentru mancare este: " << serv.suma_mancare() << endl;
+	cout << "The amount of food expenses is: " << serv.suma_mancare() << endl;
 }
 
 void ui_max_zi(Service& serv) {
-	cout << "Ziua cu cele mai mari cheltuieli este: " << serv.max_zi() << endl;
+	cout << "The day with the highest expenses is: " << serv.max_zi() << endl;
 }
 
 void ui_sume_zilnice(Service& serv) {
@@ -76,19 +76,19 @@ void ui_sume_zilnice(Service& serv) {
 
 void show_menu() {
 	cout << endl;
-	cout << "OPTIUNI: " << endl;
-	cout << "	1. Adauga cheltuiala " << endl;
-	cout << "	2. Afiseaza cheltuielile " << endl;
-	cout << "   3. Stergere cheltuiala " << endl;
-	cout << "   4. Modificare cheltuiala " << endl;
-	cout << "   5. Filtrare mancare " << endl;
-	cout << "   6. Filtrare menaj < 100 " << endl;
-	cout << "   7. Stergere cheltuieli din ziua 15" << endl;
-	cout << "   8. Afisare cheltuieli pentru mancare >5 RON" << endl;
-	cout << "   9. Stergere cheltuieli din zilele 2-9" << endl;
-	cout << "  10. Suma cheltuielilor din categoria mancare" << endl;
-	cout << "  11. Ziua cu cele mai mari cheltuieli" << endl;
-	cout << "  12. Afisare sume cheltuite zilnic descrescator" << endl;
+	cout << "OPTIONS: " << endl;
+	cout << "	1. Add expense " << endl;
+	cout << "	2. Show  expenses " << endl;
+	cout << "   3. Delete expense " << endl;
+	cout << "   4. Modify expenses " << endl;
+	cout << "   5. Filter by a expenses " << endl;
+	cout << "   6. Filter with cost< 100 " << endl;
+	cout << "   7. Delete all expenses from the 15th of the month" << endl;
+	cout << "   8. Show food expenses >5 RON" << endl;
+	cout << "   9. Delete expenses from the days 2-9" << endl;
+	cout << "  10. The amount of expenses in the food category" << endl;
+	cout << "  11. The day with the highest expenses" << endl;
+	cout << "  12. Display amounts spent daily decreasing" << endl;
 	cout << "  13. Undo" << endl;
 	cout << "	0. EXIT!" << endl;
 }
@@ -114,7 +114,7 @@ void showUI(Service& serv)
 		case 10: {ui_suma_mancare(serv); break; }
 		case 11: {ui_max_zi(serv); break; }
 		case 12: {ui_sume_zilnice(serv); break; }
-			   //case 13: {undo(serv); break; }
+		case 13: {undo(serv); break; }
 		case 0: {gata = true; cout << "EXIT!" << endl; }
 		}
 	}
